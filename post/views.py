2feed7785548a28c95663eacf27b125d2696b9eb
rook25 <bekelebrook24@gmail.com>
2025-01-04 11:00:35 +0000

    post view edits

--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
+from django.db import Q
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from user.models import User
@@ -21,7 +22,7 @@ class News(View):
 
             if index:
                 subscriptions = request.user.subscriptions.all()
-                all_posts = Post.objects.filter(Q(user__in=subscriptions) | Q(user=user)).order_by('-timestamp')
+                all_posts = Post.objects.filter(Q(user__in=subscriptions) | Q(user=request.user)).order_by('-timestamp')
                 posts = paginate_queryset(all_posts, request, PostSerializer)
 
                 return Response(posts, status=status.HTTP_200_OK)
@@ -75,7 +76,7 @@ class PostView(View):
                 #tagged_product = post_data.get('product', None)
                 if not (text): # and  tagged_product):
                     return Response({
-                            'message': 'post should have a text message and a tagged product.'}, status=400)
+                            'message': 'post should have a text message and a tagged product.'}, status=status.HTTP_400_BAD_REQUEST)
                 post = PostSerializer(data={'user': request.user, 'text': text, 'img': img})
                 if post.is_valid():
                     post.create()
@@ -84,48 +85,66 @@ class PostView(View):
             except json.JSONDecodeError as e:
                 return Response({'message': 'data could not be parsed'}, status=status.HTTP_400_BAD_REQUEST)
         
+        data = json.loads(request.body) or {}
+        text = data.get('text', '')
+        if not text:
+            return Response({'message': 'Text can\'t be empty'}, status=status.HTTP_400_BAD_REQUEST)
         elif post == 'c':
-            data = json.loads(request.body) or {}
             post_id = data.get('post_id', None)
+            post = get_object_or_404(Post, pk=post_id)
+            reply_data = {'post': post, 'text': text, 'user': request.user}
+            reply = CommentSerializer(data=reply_data)
+        elif post == 'r-c':
             comment_id = data.get('comment_id', None)
-            text = data.get('text', '')
-            if post_id and text:
-                reply_data = {'post': post, 'text': text, 'user': request.user}
-                reply = CommentSerializer(data=reply_data)
-            elif comment_id and text:
-                reply_data = {'comment': comment, 'text': text, 'user': request.user}
-                reply = ReplySerializer(data=reply_data)
-            else:
-                return Response({'message': 'proper key words missing.'}, status=401)
-            if reply.is_valid():
-                reply.create()
-            return Response({'data': comment.id,
+            comment = get_object_or_404(Comment, pk=comment_id)
+            reply_data = {'comment': comment, 'text': text, 'user': request.user}
+            reply = ReplySerializer(data=reply_data)
+        elif post == 'r-r':
+            reply_id = data.get('reply_id', None)
+            reply = get_object_or_404(Reply, pk=reply_id)
+            reply_data = {'reply': reply, 'text': text, 'user': request.user}
+            reply = ReplySerializer(data=reply_data)
+            
+        if reply.is_valid():
+            reply.create()
+        
+        return Response({'data': comment.id,
                     'message': 'comment successfully added'},
                         status=status.HTTP_200_OK)
 
     def put(self, request, post, *args, **kwargs):
             data = json.loads(request.body) or {}
             update_data = data.get('update_data', None)
-            if post == 'p' and update_data:
-                id = data.get('post_id', None)
-                post = PostSerializer(data=update_data, partial=True)
-                if post.is_valid():
-                    post.save(post_id)
 
-            elif post == 'c' and update_data:
+            if not update_data:
+                return Response('data to be updated not included', status=status.HTTP_400_BAD_REQUEST)
+            
+            if post == 'p':
+                id = data.get('post_id', None)
+                instance = get_object_or_404(Post, pk=id)
+                post = PostSerializer(instance=instance, data=update_data, partial=True)
+
+            elif post == 'c':
                 id = data.get('comment_id', None)
-                post = CommentSerializer(data=update_data, partial=True)
-            elif post == 'r' and update_data:
+                instance = get_object_or_404(Comment, pk=id)
+                post = CommentSerializer(instance=instance, data=update_data, partial=True)
+            elif post == 'r':
                 id = data.get('reply_id', None)
+                instance = get_object_or_404(Reply, pk=id)
                 post = ReplySerializer(data=update_data, partial=True)
-            else:
-                return Response({'message': 'proper key words missing.'}, status=401)
             
             if post.is_valid():
-                post.save(id)
+                post.save()
             
             return Response({'message': 'comment successfully updated'}, status=status.HTTP_200_OK)
         
     
-    def delete(self, request, post, *args, **kwargs):
-        pass
+    def delete(self, request, post, index, *args, **kwargs):
+        if not index.is_digit():
+            return Response({'message': 'index should be integer'}, status=status.HTTP_400_BAD_REQUEST)
+        model = Post if post == 'p' else Comment if post == 'c'
+                                 else Reply if post == 'r' else None
+        if model:
+            post = get_object_or_400(model, pk=index)
+            post.delete() 
+            return Response({'message': '{} succefully deleted'.format(model.__name__)}, status=HTTP_400_OK)
