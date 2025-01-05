05d79cbad4e1d2e7d4cebb1c631a09a582267199
rook25 <bekelebrook24@gmail.com>
2025-01-05 09:17:15 +0000

    cart view changes

--- cart/views.py
+++ cart/views.py
@@ -7,7 +7,7 @@ from rest_framework.respose import Response
 from rest_framwork.views import APIView
 from rest_framework import status
 import json
-from .models import Cart
+from .models import (Cart, CartData)
 from .serializers import CartSerializer
 from datetime import datetime
 # Create your views here.
@@ -25,29 +25,23 @@ class CartView(APIView):
         cart_data = json.loads(request.body)
         cart = cache.hget('cart', request.user__username)
         if cart and isinstance(cart_data, list):
-            get_list_or_404(Product, pk__in=[product.get('id', None) for product in cart_data])
+            product = get_object_or_404(Product, pk=cart_data.get('product_id', None)
             products_in_cache = json.loads(cache)
             if products_in_cache and isinstance(products_in_cache, list):
                 if len(products_in_cache) == 10:
-                    active_cache = CartSerializer(data={'user': request.user, 'status': 'active'})
-                    if active_cache.is_valid():
-                        
-                    if not active_cache:
-
-                    products_added = 
-                products_in_cache += cart_data
-                cart_updated = cache.hset('cart', request.user__username, json.dumps(products_in_cache))
-                if not cart_updated:
-                    return Response({'message': 'cart not successfully updated'}, status=status.501)
-
-            request.session.save()
-            return JsonResponse(data={'message': 'cart succefully added'}, status=200)
-                else:
-                    return JsonResponse(data={'message': 'cart name not provided'}, status=400)
-            except json.JSONDecodeError:
-                return JsonResponse(data={'message': 'json body not valid', status=400})
-        return JsonResponse(data={'message': {'No cart data recieved.'}}, status=400)
-
-
-    def delete(self, request, *args, **kwargs):
-        pass
+                    cart_data = {'user': request.user, 'status': 'active'}
+                    active_cart = CartSerializer(data=cart_data)
+                    if active_cart.is_valid():
+                        created, active_cart = Cart.objects.get_or_create(**active_cart.validated_data)
+                        if created:
+                            cart_data = CartData.objects.create(cart=active_cart).product.add(*products)
+                        else:
+                            cart_data = CartData.objects.get(cart=active_cart).product.add(*products)
+                        cache.hset('cache', request.user__username, json.dumps([]))
+                        return Response({'message': 'product successfully added to cart.'}, status=status.HTTP_200_OK)
+                cart.append(product.id)
+                added = cache.hset('cart', user__username, json.dumps(cart))
+                if added:
+                    return Response({'message': 'product succfully added'}, status=status.HTTP_200_OK)             
+        return Response({'message': 'product not successfully added. Please check your product details'},
+                            status=status.HTTP_400_BAD_REQUEST)
