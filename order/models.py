cf4c52bf7a38b805d415be64cd6abdd90e2b6ba8
Brook25 <bekelebrook24@gmail.com>
2025-06-10 18:28:45 +0300

    pg transaction table

--- order/models.py
+++ order/models.py
@@ -15,6 +15,10 @@ STATUS_TYPES = (
         ('suspended', 'Suspended')
         )
 
+PAYMENT_GATEWAYS = (
+    ('chapa', 'Chapa')
+)
+
 # Create your models here.
 
 
@@ -88,8 +92,14 @@ class ShipmentInfo(Shipment):
     def get_or_create_shipment_info(self, payment_data):
         return super.get_or_create_payment_data(payment_data)
 
-class Payment(models.Model):
+class PaymentTransaction(models.Model):
     user = ForeignKey('user.User', on_delete=models.CASCADE, related_name='payments')
     amount = DecimalField(validators=[MinValueValidator(1)], max_digits=11, decimal_places=2, null=False)
+    gateway = CharField(choices=PAYMENT_GATEWAYS)
+    trx_ref = CharField(unique=True)
+    ref_id = CharField(null=False)
     created_at = DateTimeField(auto_now=True)
     status = CharField(max_length=30, choices=STATUS_TYPES)
+
+    class Meta:
+        unique_together = ('ref_id', 'gateway')
