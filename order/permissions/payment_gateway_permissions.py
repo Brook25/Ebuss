d166ae86926db203b317af2414b5ed2fa3e53247
rook25 <bekelebrook24@gmail.com>
2025-06-10 14:36:36 +0000

    hash validation for payment gateway

--- order/permissions/payment_gateway_permissions.py
+++ order/permissions/payment_gateway_permissions.py
@@ -1,12 +1,31 @@
 from rest_framework.permissions import BasePermission
 from restframework import status
+from rest_framework.
+import os
+import hlib
+import hmac
+
 
 class IsChapa(BasePermission):
     
     def has_permission(self, request, view):
-        chapa_header = request.headers.get('Chapa-Signature', None)
+        chapa_hash = request.headers.get('Chapa-Signature', None)
 
         if not chapa_header:
-            return ('User not Authorzied to access this endpoint.', status=status.HTTP_401_UNAUTHORIZED)
+            return Response('User not Authorzied to access this endpoint.', status=status.HTTP_401_UNAUTHORIZED)
 
+       if not request.data:
+           return Response('No payload data provided.', status=status.HTTP_400_BAD_REQUEST)
         
+        secret_key = os.env.get('CHAPA_SECRET_KEY', None)
+        
+        if not secret_key:
+            return Response('authorization couldn\'t be processed.', status.HTTP_501_SERVER_ERROR)
+        
+        hash_obj = hmac.new(secret_key.encode('utf-8'), request.body, hlib.sha256)
+        hash = has_obj.hexdigest()
+
+        if hash != chapa_hash:
+            return Response('User not Authorized. Invalid Hash Key.', status=status.HTTP_401_UNAUTHORIZED)
+
+
