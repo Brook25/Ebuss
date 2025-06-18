01e96944d10f26bfb7eae46574035798ad5e2a49
Brook25 <bekelebrook24@gmail.com>
2025-06-18 12:14:22 +0300

    tasanctions satus async update

--- appstore/settings.py
+++ appstore/settings.py
@@ -238,3 +238,12 @@ MAMONA_BACKENDS_SETTINGS = {
            # 'URL': 'http://localhost:8000/'
         }
 }
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bekelebrook@gmail.com'
+EMAIL_HOST_PASSWORD = ''  # You'll need to add your app password here
+DEFAULT_FROM_EMAIL = 'bekelebrook@gmail.com'
