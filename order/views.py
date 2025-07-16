48cdc34b52861383bd6e0c10b76e216323b8361e
Brook25 <bekelebrook24@gmail.com>
2025-07-16 08:44:19 +0300

    transaction error edits in order view

--- order/views.py
+++ order/views.py
@@ -14,7 +14,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import (IsAuthenticated)
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import (CartOrderSerializer, TransactionSerializer
+from .serializers import (CartOrderSerializer, TransactionSerializer,
                           ShipmentSerializer)
 from shared.utils import paginate_queryset
 from .tasks import schedule_transaction_verification
