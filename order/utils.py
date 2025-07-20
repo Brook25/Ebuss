67dcbcf3563cc58f902637af066d146b95681ddf
Brook25 <bekelebrook24@gmail.com>
2025-07-20 11:33:43 +0300

    order error edits in order view

--- order/utils.py
+++ order/utils.py
@@ -75,7 +75,8 @@ def verify_hash_key(secret_key, request_body, hash):
     hash_obj = hmac.new(secret_key.encode('utf-8'), reserialized.encode('utf-8'), hashlib.sha256)
     
     generated_hash = hash_obj.hexdigest()
-
+    print(generated_hash)
+    print(hash)
     if hmac.compare_digest(generated_hash, hash):
         return True
     return False
